cb2306ec790e5d8dc55766ae11dcded5c977bc77
Ron Fox <foxr@msu.edu>
2019-10-14 08:09:21 -0400

    Add constructor for the fit engine.

---
+++ main/mpifit/FitEngine.cpp
@@ -0,0 +1,33 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  FitEngine.cpp
+ *  @brief: Implement base class.
+ */
+#include "jacobian.h"
+
+/**
+ * FitEngine constructor
+ *   Just marshall the x/y points.
+ */
+FitEngine::FitEngin(std::vector<uint16_t, uint16_t>& data)
+{
+    for (size_t i = 0; i < data.size(); i++) {
+        x.push_back(data[i].first);
+        y.push_back(data[i].second);
+    }
+}
