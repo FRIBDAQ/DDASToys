2c285354a41e8f81c99ae6bcf4c9b25db7ca1728
Chester, Aaron <chestera@msu.edu>
2026-08-20 19:17:55 +0000

    Specify analyticfit chisq in cudafit

--- cudafit_analytic.cu
+++ cudafit_analytic.cu
@@ -518,9 +518,9 @@ void ddastoys::analyticfit::cudafit1(
   pResult->pulse.steepness = pParams[K1];
   pResult->pulse.decayTime = pParams[K2];
 
-  pResult->chiSquare =
-      chiSquare1(pParams[A1], pParams[K1], pParams[K2], pParams[X1], pParams[C],
-                 trace, limits.first, limits.second);
+  pResult->chiSquare = analyticfit::chiSquare1(
+      pParams[A1], pParams[K1], pParams[K2], pParams[X1], pParams[C], trace,
+      limits.first, limits.second);
 }
 
 void ddastoys::analyticfit::cudafit2(
@@ -583,8 +583,8 @@ void ddastoys::analyticfit::cudafit2(
   pResult->pulses[1].amplitude = pParams[A2];
   pResult->pulses[1].steepness = pParams[K3];
   pResult->pulses[1].decayTime = pParams[K4];
-  pResult->chiSquare =
-      chiSquare2(pParams[A1], pParams[K1], pParams[K2], pParams[X1],
-                 pParams[A2], pParams[K3], pParams[K4], pParams[X2], pParams[C],
-                 trace, limits.first, limits.second);
+  pResult->chiSquare = analyticfit::chiSquare2(
+      pParams[A1], pParams[K1], pParams[K2], pParams[X1], pParams[A2],
+      pParams[K3], pParams[K4], pParams[X2], pParams[C], trace, limits.first,
+      limits.second);
 }
